90d6f3961a90cfb9c74ee4bce3332ce7cdb51cf4
konno57 <126854839+konno57@users.noreply.github.com>
2023-03-11 09:17:05 +0000

    [Add]bookのindexレイアウト作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,24 @@
 class BooksController < ApplicationController
   def index
-  
+    @books = Book.order(:id)
   end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
 end
